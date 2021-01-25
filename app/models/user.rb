943a22dbbd34713398e6f4ad10fd71d6d4d4f1bb
Isamu Hosoda <elchsd2020@gmail.com>
2021-01-25 20:20:14 +0000

    Follow function completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  # 経路1
+  # uesr(user_id) → 中間テーブル → target_user(user_id)
+  # 中間テーブル本体はforrow
+  has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
+  
+  # 経路2
+  # uesr(user_id) ← relationships ← target_user(user_id)
+  # 中間テーブル本体はforrow
+  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_user_id'
+  
+  has_many :followings, through: :active_relationships, source: :target_user
+  has_many :followers, through: :passive_relationships, source: :user
+  
   has_many :photos
 end
