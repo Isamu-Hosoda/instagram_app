2c71d7fd46ee2b2986d470cb16350b9b74edbac9
Isamu Hosoda <elchsd2020@gmail.com>
2021-01-30 14:50:39 +0000

    Comment function completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  resources :photos
+  resources :photos do
+    resources :comments
+  end
+  
   resources :users do
     resource :follow
     resources :followings
