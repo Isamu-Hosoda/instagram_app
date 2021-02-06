9a523d3d922a5851d84224e4c1502d0f536defb6
Isamu Hosoda <elchsd2020@gmail.com>
2021-02-06 00:10:22 +0000

    Photo deletion function completed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :favorites
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
   has_one_attached :image
   
   validates :image, presence: true
